099e17cca10395ce0c6b0f268b5fbded9afb4851
Alagbajohn <alagbajohn100@gmail.com>
2025-05-02 14:59:22 -0700

    Update App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+// variebles
+const fullName = "Ezekiel John";
+
+let age = 36;
+
+const dating = false;
+
+const hobby= [ football "FIFA
+
+const biodata = { 
+    height: "174cm"
+    weigh: 84;
+    blood:"B+;
+    gen: "AA
+}
