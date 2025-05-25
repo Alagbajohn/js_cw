b3c6ab093219b6deb31f48321541ded430bbc814
Alagbajohn <alagbajohn100@gmail.com>
2025-05-25 06:16:27 -0700

    js_cw

---
+++ dom.js
@@ -0,0 +1,16 @@
+console.log("working");
+const header = document. getElementById("header");
+
+console.log(header);
+console.dir(header);
+console.log(header.className);
+console.log
+
+function changeBackground(){
+    header.style.
+    backgroundcolor ="red";
+    header.style.color = "white";
+    header.style.fontSize = "50px";
+}
+
+header.addEventListener()
